d8e2897d0c94d13e900b3e9d5738d6cd30662dfa
Mike Cooper <mikescooper@icloud.com>
2019-08-31 18:35:20 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,5 +23,17 @@ class Book
   def page_count
     @page_count
   end 
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def genre
+    @genre
+  end 
+  
+  def turn_page
+    
+  end
 
 end 
